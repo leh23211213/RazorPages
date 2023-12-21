a944794e44b52a7cbf907631eccbff34838d4783
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2023-12-21 09:38:25 +0700

    update Routing

---
+++ Startup.cs
@@ -0,0 +1,51 @@
+namespace Razor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+            .AddRazorPages()
+            .AddRazorPagesOptions(options =>
+            {
+                options.RootDirectory = "/Pages";
+                options.Conventions.AddPageRoute("/", "/");
+            });
+        }
+
+
+
+        public IConfiguration Configuration { get; }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
